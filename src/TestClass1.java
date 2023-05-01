dc9b463cc37f29cd0f7579ad57917ecd05dac047
PrabhatSuyal <prabhatsuyal20@gmail.com>
2023-05-01 15:33:22 +0530

    added stmt 3 locally

--- src/TestClass1.java
+++ src/TestClass1.java
@@ -1,6 +1,5 @@
 public class TestClass1 {
     public static void funTestClass1(){
         System.out.println("stmt 1 funTestClass1");
-        System.out.println("stmt 2 funTestClass1");
     }
 }
