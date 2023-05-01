490cc14f9422c3bfed7bf07ba6e3480a1011a745
PrabhatSuyal <prabhatsuyal20@gmail.com>
2023-05-01 14:21:38 +0530

    new file created

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("2nd string");
         System.out.println("Hello world!");
         System.out.println("2nd string");
+        TestClass1.funTestClass1();
     }
 }
