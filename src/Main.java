af5a6996ef0b69930d66b0f4bb4555999d492a13
PrabhatSuyal <prabhatsuyal20@gmail.com>
2023-05-01 14:08:50 +0530

    inserted some more print statements

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        
+        System.out.println("2nd string");
+        System.out.println("Hello world!");
+        System.out.println("2nd string");
+        System.out.println("Hello world!");
+        System.out.println("2nd string");
+        System.out.println("Hello world!");
+        System.out.println("2nd string");
     }
 }
